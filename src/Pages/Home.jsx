dd2228fec5ff821118ec8e311f2712621e2e75d3
hashiq <mohdhashique10@gmail.com>
2023-11-24 18:24:10 +0530

    fix layout

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,20 +7,41 @@ import Overview from "../Components/Overview";
 
 function Home() {
   return (
+    // <div>
+    //   <Navbar />
+    //   <div className="grid [grid-template-columns:25%_75%] gap-4">
+    //     <div className="border  border-black">
+    //       <Account className="w-auto" />
+    //     </div>
+    //     <div className="border border-black">
+    //       <Analysis />
+    //     </div>
+    //     <div className="border border-black ">
+    //       <Overview className="w-auto" />
+    //     </div>
+    //     <div className="border border-black p-1">
+    //       <Transation />
+    //     </div>
+    //   </div>
+    // </div>
     <div>
       <Navbar />
-      <div className="">
-        <div className="border border-black m-3 w-3/4 h-3/4">
-          <Account className="w-auto" />
+      <div className="flex flex-wrap gap-3 p-1">
+        <div className="w-1/4 flex flex-col gap-3">
+          <div className="border  border-black">
+            <Account className="w-auto" />
+          </div>
+          <div className="border border-black ">
+            <Overview className="w-auto" />
+          </div>
         </div>
-        <div className="border border-black m-3">
-          <Analysis />
-        </div>
-        <div className="border border-black m-3 w-3/4">
-          <Overview className="w-auto" />
-        </div>
-        <div className="border border-black m-3 h-3/4 p-1">
-          <Transation />
+        <div className="max-w-full flex-1 flex flex-col gap-3">
+          <div className="border border-black">
+            <Analysis />
+          </div>
+          <div className="border border-black p-1">
+            <Transation />
+          </div>
         </div>
       </div>
     </div>
